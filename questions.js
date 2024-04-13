6aaa2125b683b400706b2ee1856fcf57954b4f53
FXDCommunity <166769846+FXDCommunity@users.noreply.github.com>
2024-04-13 02:24:05 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,77 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 26,
+        question: "Mario Bros Termasuk Game?",
+        answer: "Indie",
+        options: ["Pixel", "Indie", "3D", "2D"]
+    },
+    {
+        numb: 27,
+        question: "Bioskop Simulator Di Kembangkan Oleh?",
+        answer: "Andy Lukito",
+        options: ["Akhir Pekan Studio", "Andy Lukito", "Kajewdev", "Livander Gamedev"]
+    },
+    {
+        numb: 28,
+        question: "Kepanjangan Game BUSSID Adalah?",
+        answer: "Bus Simulator Indonesia",
+        options: ["Bus Simulator Indie", "Bus Simulator Indonesia", "Bus Simulator Index", "Bus Simulator India"]
+    },
+    {
+        numb: 29,
+        question: "Game Yang Sedang Di Kembangkan Andy Lukito Sekarang Ini Adalah?",
+        answer: "Warnet Life 2",
+        options: ["Bioskop Simulator", "Warnet Life", "Warnet Life 2", "Kantin Sekolah Simulator"]
+    },
+    {
+        numb: 30,
+        question: "Aku Si Peternak Lele DI Kembangkan Oleh?",
+        answer: "Kajewdev",
+        options: ["Andy Lukito", "Livander Gamedev", "Kajewdev", "Lainnya"]
+    },
+    {
+        numb: 31,
+        question: "Game Yang",
+        answer: "",
+        options: ["", "", "", ""]
+    },
+    {
+        numb: 32,
+        question: "",
+        answer: "",
+        options: ["", "", "", ""]
+    },
+    {
+        numb: 33,
+        question: "",
+        answer: "",
+        options: ["", "", "", ""]
+    },
+    {
+        numb: 34,
+        question: "",
+        answer: "",
+        options: ["", "", "", ""]
+    },
+    {
+        numb: 35,
+        question: "",
+        answer: "",
+        options: ["", "", "", ""]
+    },
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
